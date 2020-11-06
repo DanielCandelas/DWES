f8aab334b11ebcc38bf9840fefef0c25012f1451
Daniel Candelas <danielcandelas98@gmail.com>
2020-11-06 17:43:05 +0100

    kk

--- 7. Cokkies y Sesiones/Ejercicio18/vistas/validacion.php
+++ 7. Cokkies y Sesiones/Ejercicio18/vistas/validacion.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "<form action='' method='post'>";
-echo "dni: <input type='text' name='dni'><br/>";
-echo "Contraseña: <input type='text' name='pass'><br/>";
+echo "<form action='' method='POST'>";
+echo "<br> DNI: <input type='text' name='dni'><br/>";
+echo "<br> Contraseña: <input type='text' name='pass'><br/>";
 echo "<input type='submit' name='enviarPass'>";
 echo "</form>";
