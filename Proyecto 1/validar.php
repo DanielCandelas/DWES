b058290bb328e04953e1bc787143de5a908959e7
Daniel Candelas <danielcandelas98@gmail.com>
2020-11-14 13:36:25 +0100

    ok

--- Proyecto 1/validar.php
+++ Proyecto 1/validar.php
@@ -6,29 +6,35 @@
     require "modelo.php";
     $base = new Bd();    
 
-    if (isset($_POST['enviarPwd'])){   //Si hemos enviado el formulario de validacion comprobamos dni y pwd       
-        $cli = new Cliente($_POST['dni'],'','','',$_POST['pwd'],'');
+    if (!isset($_SESSION['nombre'])){
+        if (isset($_POST['enviarPwd'])){   //Si hemos enviado el formulario de validacion comprobamos dni y pwd       
+            $cli = new Cliente($_POST['dni'],'','','',$_POST['pwd'],'');
 
-        if($nom = $cli->autenticar($base->link)){   //Mediante un metodo autenticar comprobamos si el usuario y la contraseña son correctas
-            
-            if($admin = $cli->soyAdmin($base->link)){  //Comprobamos si el usuario es administrador o no lo es
-                //Si es administrador hacemos header a CRUD de cliente.
+            if($nom = $cli->autenticar($base->link)){   //Mediante un metodo autenticar comprobamos si el usuario y la contraseña son correctas
                 
-            } else { //Si no es adminstrador creamos 3 variables de sesion y hacemos header a principal.php
-                $_SESSION['nombre'] = $nom['nombre']; 
-                $_SESSION['dni'] = $_POST['dni'];
-                $_SESSION['total'] = 0;
-                header('Location: principal.php');
+                if($admin = $cli->soyAdmin($base->link)){  //Comprobamos si el usuario es administrador o no lo es
+                    //Si es administrador hacemos header a CRUD de cliente.
+                    
+                } else { //Si no es adminstrador creamos 3 variables de sesion y hacemos header a principal.php
+                    $_SESSION['nombre'] = $nom['nombre']; 
+                    $_SESSION['dni'] = $_POST['dni'];
+                    $_SESSION['total'] = 0;
+                    header('Location: principal.php');
+                }
+
+            } else {
+                //El usuario o cantraseña es incorrecto por lo que vuelve a validar.
+                $mensaje = "Usuario o contraseña incorrectos <br>";
+                $mensaje .= "<a href='validar.php'> Volver </a>";
+                require "vistas/mensaje.php";
             }
 
-        } else {
-            //El usuario o cantraseña es incorrecto por lo que vuelve a validar.
-            $mensaje = "Usuario o contraseña incorrectos <br>";
-            $mensaje .= "<a href='validar.php'> Volver </a>";
-            require "vistas/mensaje.php";
-        }
+        } else require "vistas/validacion.php";  
+    }  
 
-    } else require "vistas/validacion.php";    
+    if (isset($_SESSION['nombre'])){
+        header('Location: principal.php');
+    }
     
     require "vistas/fin.html";
 
