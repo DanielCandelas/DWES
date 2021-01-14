dd5f13ae91e610fb6fb0a707fdde740a7b11c360
Daniel Candelas <danielcandelas98@gmail.com>
2021-01-14 11:09:51 +0100

    pdo

---
+++ 9. PDO/Ejercicio 20/modelo.php
@@ -0,0 +1,30 @@
+<?php
+
+class Bd {
+    private $link;
+	function __construct()
+	{
+		if (!isset ($this->link)) {
+			try{
+				$this->link= new PDO("mysql:host=localhost;dbname=virtualmarket", "root", "");
+			}
+			catch(PDOException $e){
+				$dato= "¡Error!: " . $e->getMessage() . "<br/>";
+ 				require "vista/mensaje.php";
+ 				die();
+ 			}
+ 		}
+	}
+		
+	function __get($var){
+		return $this->$var;
+	}
+} 
+
+class Pedido {
+
+}
+
+class Linea {
+
+}
