b6482a7939e8a91c1dbc0479af4a30dc1dfbecd1
Daniel Candelas <danielcandelas98@gmail.com>
2021-02-02 16:40:18 +0100

    ok

--- 10-ServiciosWeb/ej25/funcion.php
+++ 10-ServiciosWeb/ej25/funcion.php
@@ -1,7 +1,7 @@
 <?php
 
 function lista ($link,$url,$tabla, $nomid, $mostrar){
-	$url="http://localhost/ej25/".$url.$tabla;
+	$url="http://localhost/DWES-2021/10-Serviciosweb/ej25/".$url.$tabla;
     $consulta=json_decode(file_get_contents($url),TRUE);
     $string= "<select name='$tabla'>";
     foreach ($consulta as $fila) {
